ee51f34b8f898813126085da3d802300ce106533
Adam Ray <raywibowo68@gmail.com>
2022-05-28 08:22:23 +0700

    Add 3 NavItem, create Navbar and NavItem component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from "react";
 
 function App() {
-  return <Navbar></Navbar>;
+  return (
+    <Navbar>
+      <NavItem />
+    </Navbar>
+  );
 }
 
-function Navbar() {
+function Navbar(props) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav"></ul>
+      <ul className="navbar-nav">
+        {props.children}
+      </ul>
     </nav>
   );
+}
+
+function NavItem() {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+      </a>
+    </li>
+  )
 }
 
 export default App;
